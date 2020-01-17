eaa7377180179d331129f80e9d90f61760dad5eb
Gabriela <gabiprds.93@gmail.com>
2020-01-16 22:25:28 -0500

    Updates

---
+++ precache-manifest.398f5e7e56204de643cde34f93fdb1f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f803edbe7e9e0e09b3629d84158a525f",
+    "url": "/Cotizar-Seguro-Salud/index.html"
+  },
+  {
+    "revision": "fb8e6f21a9fc36944deb",
+    "url": "/Cotizar-Seguro-Salud/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1b9a550709cda0419d2a",
+    "url": "/Cotizar-Seguro-Salud/static/js/2.0d278baa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Cotizar-Seguro-Salud/static/js/2.0d278baa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb8e6f21a9fc36944deb",
+    "url": "/Cotizar-Seguro-Salud/static/js/main.900b5df6.chunk.js"
+  },
+  {
+    "revision": "d76bb190cb952b7d183f",
+    "url": "/Cotizar-Seguro-Salud/static/js/runtime-main.f7cc77dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cotizar-Seguro-Salud/static/media/logo.5d5d9eef.svg"
+  }
+]);
