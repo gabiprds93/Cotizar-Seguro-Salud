26514b2370d31f588f23f3aafcb99c5bdd746834
Gabriela <gabiprds.93@gmail.com>
2020-01-17 02:59:47 -0500

    add  component InsuranceForm and some styles

---
+++ src/components/InsuranceForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InsuranceForm = () => {
+  return(
+    <div>
+      <h3><span className='txt-black'>Obtén tu </span><span className='txt-red'>seguro ahora</span></h3>
+      <p>Ingresa los datos para comenzar</p>
+    </div>
+  )
+}
+
+export default InsuranceForm
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import InsuranceForm from '../components/InsuranceForm'
+
+const Home = () => {
+  return(
+    <div>
+      <InsuranceForm></InsuranceForm>
+    </div>
+  )
+}
+
+export default Home
