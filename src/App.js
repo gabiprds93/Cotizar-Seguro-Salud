043eb2d73561c51eb1f492efe9985a2cd08b7914
Gabriela <gabiprds.93@gmail.com>
2020-01-17 02:24:31 -0500

    add component button and scss variables and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/styles/App.scss';
+
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
+      <Button></Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
