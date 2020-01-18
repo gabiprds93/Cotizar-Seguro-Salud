fc129a68d3c4cf7811d889c088b1377544ab1cb3
Gabriela <gabiprds.93@gmail.com>
2020-01-18 11:34:12 -0500

    add grid system

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-grid-system';
 
 import InsuranceForm from '../components/InsuranceForm'
 import InsuranceInfo from '../components/InsuranceInfo'
 
 const Home = () => {
   return(
-    <div>
-      <InsuranceInfo></InsuranceInfo>
-      <InsuranceForm></InsuranceForm>
-    </div>
+    <Container fluid style={{padding: 0}}>
+      <Row nogutter>
+        <Col md={6}>
+          <InsuranceInfo />
+        </Col>
+        <Col>
+          <InsuranceForm />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
