bbc69d564c83e92225681d9250c1f64f845ba097
Gabriela <gabiprds.93@gmail.com>
2020-01-19 15:19:14 -0500

    fixing styles

--- src/components/InsuranceFormConfig.js
+++ src/components/InsuranceFormConfig.js
@@ -3,16 +3,6 @@ export const InsuranceFormConfig = [
   { label: 'Nro. de Documento', type: 'text', name: 'numDocumento', required: true, size: 8 },
   { label: 'Fecha de Nacimiento', type: 'date', name: 'fecNacimiento', required: true, size: 12, },
   { label: 'Celular', type: 'text', name: 'telefono', required: true, size: 12, },
-  {
-    label: 'Acepto la ',
-    type: 'checkbox',
-    name: 'terms',
-    link: 'Política de Protección de Datos Personales y los Términos y Condiciones.',
-    redirect: '/terminos-condiciones',
-    checked: true,
-    display: 'block',
-    size: 12,
-  },
 ]
 
 export const tipoDocumento = [
