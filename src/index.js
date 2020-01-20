9ec3a191b7648a37c8eb5418458ccecad9429fe9
Gabriela <gabiprds.93@gmail.com>
2020-01-19 21:41:01 -0500

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
 import './index.css';
 import store from './redux/store'
 import App from './App';
 import Home from './pages/Home'
+import Steps from './pages/Steps'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/pasos" component={Steps} />
+      </Switch>
+    </BrowserRouter>
   </Provider>, 
   document.getElementById('root')
 );
